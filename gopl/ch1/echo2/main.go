1a2be2eb8a3bc6def1e32cae2a542b57d053bd81
Larry Xu <hilarryxu@gmail.com>
2021-10-18 15:38:09 +0800

    feat(gopl): add ch1/echo

---
+++ gopl/ch1/echo2/main.go
@@ -0,0 +1,17 @@
+// Package main provides ...
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var s, sep string
+
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
